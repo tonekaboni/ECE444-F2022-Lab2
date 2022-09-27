5d9d018e2642ae5d9e893b0d8faaaeb6695beab1
SadraIBM <sadra.tonekaboni@ibm.com>
2022-09-27 19:06:17 -0400

    Activity 4 completed

--- hello.py
+++ hello.py
@@ -1,30 +1,42 @@
-from datetime import datetime
-from flask import Flask, render_template
+
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.validators import Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+class infoForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your Uoft Email Address?', validators=[DataRequired(),Email('please enter a valid email containing @')])
+    submit = SubmitField('Submit')
 
 
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    form = infoForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        email_in = form.email.data
+        old_email = session.get('email')
+        if old_email is not None and old_email != email_in:
+            flash('Looks like you have changed your email!')
+        if 'utoronto.ca' in email_in:
+            session['email'] = email_in
+        else:
+            #please keep in mind that invalid cannot be the address itself as the email validator will catch it
+            session['email'] = 'invalid'
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),email=session.get('email'))
